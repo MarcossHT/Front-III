138a4fab183cd2cdc437b8351f487b69661655b8
MarcossHT <mppiress@gmail.com>
2022-10-24 22:26:01 -0300

    feat: aula 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { SegundaAula } from './aulas/SegundaAula'
-import { TerceiraAula } from './aulas/TerceiraAula'
-import { Login } from './pages/Login/index'
+import { QuartaAula } from './aulas/QuartaAula'
+// import { SegundaAula } from './aulas/SegundaAula'
+// import { TerceiraAula } from './aulas/TerceiraAula'
+// import { Login } from './pages/Login/index'
 
 function App() {
   
   return (
     <>
-      {/* <Login />
-      <SegundaAula /> */}
-      <TerceiraAula />
+      <QuartaAula />
     </>
   )
 }
---
+++ src/aulas/QuartaAula/index.jsx
@@ -0,0 +1,42 @@
+import './style.scss';
+import image from './../../assets/images/geforce-ada-4090-web-og-1200x630@2x.jpg'
+import image2 from './../../assets/images/Untitled1044.jpg'
+
+export function QuartaAula() {
+
+    const componentsFinded = [
+        {
+            title: 'Vídeo Component',
+            image: image,
+            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, commodi?'
+        },
+        {
+            title: 'Input Component',
+            image: image2,
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, id.'
+        }
+    ];
+
+    return (
+        <div className="quarta-aula-component">
+            <h1 className="main-title">Componentes Identificados</h1>
+
+            <ul className="components-finded">
+
+                {
+                    componentsFinded.map(c => {
+                        return (
+                            <li>
+                                <img src= {c.image} alt=""></img>
+                                <h1>{c.title}</h1>
+                                <p>{c.text}</p>
+                            </li>
+                        )
+                        
+                    })
+                }
+
+            </ul>
+        </div>
+    )
+}
